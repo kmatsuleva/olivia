c49fd82542aa42f72da6bec43bf6c39d726c7d6a
Katerina Matsuleva <katerina.matsuleva@eveliko.com>
2021-12-21 17:28:30 +0200

    Added initial structure of Swiper

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AdventureCardsList from './components/Cards/AdventureCardsList';
 import adventureCards from './mocks/adventure-cards';
 import Section from './components/General/Section';
 import ContactUsForm from './components/Form/ContactUsForm';
+import Testimonials from './components/Testimonials/Testimonials';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
       </Section>
       <Section heading="Contact Us" isFullWidth={false} isCentered={false}>
         <ContactUsForm />
+      </Section>
+      <Section subheading="Testimonials" heading="Client success stories" isFullWidth={false} isCentered={false}>
+        <Testimonials />
       </Section>
     </>
   );
