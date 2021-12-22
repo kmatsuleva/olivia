cf02d130bd3ffbea66d390de2ade427411a225e7
Katerina Matsuleva <katerina.matsuleva@eveliko.com>
2021-12-22 14:01:51 +0200

    Added images to the cards

--- src/mocks/aventure-cards.js
+++ src/mocks/aventure-cards.js
@@ -1,18 +1,21 @@
 const aventureCards = [
     {
         id: 1,
+        image: 'https://images.unsplash.com/photo-1584465889075-91d745be4a3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80',
         title: 'Lorem ipsum',
         summary: 'Dolor sit amet, consectetur adipiscing elit. Nullam ut massa lacus. Aliquam erat enim, viverra sed justo eget, dictum ornare nibh.',
         cta: 'Read More'
     },
     {
         id: 2,
+        image: 'https://images.unsplash.com/photo-1584465889075-91d745be4a3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80',
         title: 'Aenean a sodales',
         summary: 'Integer elit turpis, mattis at sem et, finibus pulvinar felis. Ut dolor felis, convallis ac mollis eu, ultrices id est.',
         cta: 'Read More'
     },
     {
         id: 3,
+        image: 'https://images.unsplash.com/photo-1584465889075-91d745be4a3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80',
         title: 'Fusce pulvinar',
         summary: ' Aliquam auctor enim non urna eleifend, in congue turpis placerat. Donec nulla libero, facilisis a neque sed, pretium scelerisque neque.',
         cta: 'Read More'
