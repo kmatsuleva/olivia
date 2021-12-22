7b947424637664a851292aeddd6a22d81804cf03
Katerina Matsuleva <katerina.matsuleva@eveliko.com>
2021-12-22 09:33:28 +0200

    Added more options to the Swiper

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,20 +1,41 @@
 import React from 'react';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+
+import 'swiper/swiper.scss';
+import 'swiper/modules/autoplay/autoplay.scss';
+import 'swiper/modules/navigation/navigation.scss';
+
 import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js';
-import 'swiper/swiper.scss'; 
+import SwiperCore, { Autoplay, Navigation } from "swiper";
+SwiperCore.use([Autoplay, Navigation]);
 
 const Testimonials = () => {
   return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-    </Swiper>
+    <Row>
+      <Col md={6}>
+        <Swiper
+          autoplay={{
+            "delay": 5000,
+            "disableOnInteraction": false
+          }}
+          centered={true}
+          navigation={true}
+          pagination={{
+            "dynamicBullets": true
+          }}
+          loop={true}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+        </Swiper>
+      </Col>
+    </Row>
+
   );
 };
 
