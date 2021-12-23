8a179ba89f5d00536dfad44557ef23b36050f4a9
Katerina Matsuleva <katerina.matsuleva@eveliko.com>
2021-12-23 15:20:08 +0200

    Added PropTypes and styles for the Testimonials component

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Row, Col } from 'react-bootstrap';
 
 import "swiper/css";
@@ -34,5 +35,9 @@ const Testimonials = ({ testimonials }) => {
     </Row>
   );
 };
+
+Testimonials.propTypes = {
+  cardsList: PropTypes.array
+}
 
 export default Testimonials;
